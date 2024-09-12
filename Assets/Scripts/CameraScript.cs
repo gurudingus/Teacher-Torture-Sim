bc4e5c6879ba86e68f56340e02d8d1f497c0287e
Dundette <136685603+Dundette@users.noreply.github.com>
2024-09-12 19:08:23 +1000

    Finished computer interface
    Computer interface is functional, no functionality has been added and it still uses placeholder art, but you know...baby steps.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -12,21 +12,72 @@ public class CameraScript : MonoBehaviour
 
     public float pitchRange = 60f;
 
+    public GameObject player;
+
+    public Transform camPos;
+    public Transform playerPos;
+
+    private int camState = 0;
+
     private void Awake()
     {
         firstPersonCam = GetComponentInChildren<Camera>();
     }
+
+    private void Start()
+    {
+       Cursor.lockState = CursorLockMode.Locked;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (camState == 0)
+        {
+            moveToPos(0);
             //rotate player around
             float rotateYaw = Input.GetAxis("Mouse X") * mouseSensitivity;
-            transform.Rotate(0, rotateYaw, 0);
+            player.transform.Rotate(0, rotateYaw, 0);
 
             //rotate cam up and down
             rotateCameraPitch += -Input.GetAxis("Mouse Y") * mouseSensitivity;
             //lock rotation so no flip camera
             rotateCameraPitch = Mathf.Clamp(rotateCameraPitch, -pitchRange, pitchRange);
             firstPersonCam.transform.localRotation = Quaternion.Euler(rotateCameraPitch, 0, 0);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                camState = 1;
+                Cursor.lockState = CursorLockMode.None;
+                player.GetComponent<CharacterControler>().enabled = false;
+                player.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+        else if (camState == 1)
+        {
+            moveToPos(1);
+            if (Input.GetButtonDown("Jump"))
+            {
+                camState = 0;
+                Cursor.lockState = CursorLockMode.Locked;
+                player.GetComponent<CharacterControler>().enabled = true;
+                player.GetComponent<MeshRenderer>().enabled = true;
+            }
+            
+        }
+
+    }
+
+    private void moveToPos(int position)
+    {
+        if (position == 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, playerPos.position, Time.deltaTime * 10);
+        }
+        else if (position == 1)
+        {
+            transform.position = Vector3.Lerp(transform.position, camPos.position, Time.deltaTime * 10);
+            transform.rotation = Quaternion.Euler(0f,0f,0f);
+        }
     }
 }
