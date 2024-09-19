afa167fae664bc0d83537e281df99086140fcce1
SomeOtherCyclist <realliam.shelley@gmail.com>
2024-09-19 14:50:22 +1000

    Door lock
    - Added a door lock boolean to the door. Currently no way to set locked though

--- Assets/Scripts/Environment/Door.cs
+++ Assets/Scripts/Environment/Door.cs
@@ -7,12 +7,16 @@ public class Door : MonoBehaviour, IInteractable {
     [SerializeField] [Tooltip("Opening time in seconds")] private float openingTime = 0.5f;
     private float openingSpeed = 180f;
 
+    private bool doorLocked = false;
+
     private void Awake() => openingSpeed = openingAngle / openingTime; //Also make sure it's set properly on awake
     private void OnValidate() => openingSpeed = openingAngle / openingTime; //Make sure this gets updated whenever you change the values in the inspector
 
     public void Interact()
     {
-        StopAllCoroutines(); //Cancel all coroutines 
+        if (doorLocked) return; //Cancel all further interaction if the door is locked
+
+        StopAllCoroutines(); //Cancel all coroutines to make sure you don't get a lock halfway through where both coroutines are forever pulling the door open and closed at the same time
 
         if (angle < openingAngle * 0.5f) StartCoroutine(Open()); //If the door is at least mostly closed, attempt to open it
         if (angle > openingAngle * 0.5f) StartCoroutine(Close()); //If the door is at least mostly open, attempt to close it
