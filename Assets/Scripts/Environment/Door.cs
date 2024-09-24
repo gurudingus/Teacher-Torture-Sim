2775889f668959965f2fe5f2b1b1e8a8a02adc52
LiamShelley <66814870+SomeOtherCyclist@users.noreply.github.com>
2024-09-24 19:07:06 +1000

    Undid moving to animations for the door
    God I hate animations

--- Assets/Scripts/Environment/Door.cs
+++ Assets/Scripts/Environment/Door.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
-[RequireComponent(typeof(Animator))] public class Door : MonoBehaviour, IInteractable {
-    [SerializeField] [Tooltip("Opening time in seconds")] private float openingTime = 0.5f;
-    private bool isOpen = false;
-
-    private Animator animator;
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField][Tooltip("Opening time in seconds")] private float openingTime = 0.5f;
+    [SerializeField][Tooltip("Opening angle in degrees")] private float openingAngle = 90f;
+    private float openingSpeed = 180f;
+    private float angle = 0;
 
     private bool locked = false;
     public bool Locked
@@ -12,23 +14,47 @@ using UnityEngine;
         set
         {
             locked = value;
+            if (locked) angle = 0;
             //if (locked) angle = 0;
         }
     }
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-        animator.speed = 1f / openingTime; //Set the opening speed
-    }
-
-    private void OnValidate() => GetComponent<Animator>().speed = 1f / openingTime;
+    private void Awake() => openingSpeed = openingAngle / openingTime; //Also make sure it's set properly on awake
+    private void OnValidate() => openingSpeed = openingAngle / openingTime; //Make sure this gets updated whenever you change the values in the inspector
 
     public void Interact(InteractionScript source)
     {
         if (locked) return; //Cancel all further interaction if the door is locked
 
-        isOpen = !isOpen; //Toggle whether or not the door is open
-        animator.Play(isOpen ? "Door Close" : "Door Open", 0, 0f); //Play the animation
+        StopAllCoroutines(); //Cancel all coroutines to make sure you don't get a lock halfway through where both coroutines are forever pulling the door open and closed at the same time
+
+        if (angle < openingAngle * 0.5f) StartCoroutine(Open()); //If the door is at least mostly closed, attempt to open it
+        if (angle > openingAngle * 0.5f) StartCoroutine(Close()); //If the door is at least mostly open, attempt to close it
+    }
+
+    IEnumerator Open()
+    {
+        while (angle < openingAngle) //While the angle is under the maximum, move towards the maximum and update the transform
+        {
+            angle += Time.deltaTime * openingSpeed;
+            if (angle > openingAngle) angle = openingAngle; //Guard to make sure it doesn't go past the bounds
+
+            transform.eulerAngles = new(0, angle, 0);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator Close()
+    {
+        while (angle > 0f) //Exact same thing but with closing
+        {
+            angle -= Time.deltaTime * openingSpeed;
+            if (angle < 0f) angle = 0f;
+
+            transform.eulerAngles = new(0, angle, 0);
+
+            yield return null;
+        }
     }
 }
