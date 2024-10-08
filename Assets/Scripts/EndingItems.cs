bfed7c20f7c0b2ee0a85ceb71c40d6aab9a0d62b
SomeOtherCyclist <realliam.shelley@gmail.com>
2024-10-08 15:33:07 +1100

    Started the script that "spawns" items depending on which levels you have completed

---
+++ Assets/Scripts/EndingItems.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EndingItems : MonoBehaviour {
+
+    [SerializeField] private GameObject[] endingObjects = new GameObject[9];
+
+    private void Awake() => SpawnItems();
+
+    public void SpawnItems()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (!Events.GetEventComplete((GameEvent)i)) Destroy(endingObjects[i]);
+        }
+    }
+}
