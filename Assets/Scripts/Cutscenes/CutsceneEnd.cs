93a268b50a52bb3d0296cf6bbe4f1e590c09bbb3
Dundette <136685603+Dundette@users.noreply.github.com>
2024-10-17 13:16:26 +1100

    Cutscene 6 and credits fixup
    Fixed up credits and cutscene 6

--- Assets/Scripts/Cutscenes/CutsceneEnd.cs
+++ Assets/Scripts/Cutscenes/CutsceneEnd.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class CutsceneEnd : MonoBehaviour {
 
     public void ReloadLevel() => GameManager.LoadLevel(0);
+    public void loadCredits() => GameManager.LoadLevel(1);
 }
